17f7c19f68a91e2adf4f88b8eae53ca7f22cb03d
alexfun45 <alexfun45@gmail.com>
2023-03-30 17:29:10 +0300

    Добавление ссылки на видео в редакторе страниц

--- src/components/SummerEditor.js
+++ src/components/SummerEditor.js
@@ -56,7 +56,7 @@ export default (props) => {
             ['fontname', ['fontname']],
             ['para', ['ul', 'ol', 'paragraph', 'height']],
             ['table', ['table']],
-            ['insert', ['link', 'picture']],
+            ['insert', ['link', 'picture', 'video']],
             ['view', ['fullscreen', 'codeview']],
             ['height', ['height']],
             ['misc', ['undo', 'redo']]
